eb8439efd809771012bb1520067b150efe9fdee4
AymericM <aymeric.moehn@gmail.com>
2018-04-09 11:06:45 +0200

    code structure

---
+++ js/classes/champs.js
@@ -0,0 +1,13 @@
+class Champs {
+    constructor() {
+        this.state = {
+            test: 'Hello'
+        }
+    }
+
+    test() {
+        return this.state
+    }
+}
+
+export { Champs }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import { store, storeActions } from './store/GameStore.js'
+import { Champs } from './classes/champs.js'
 
+// class demo
+
+const champ = new Champs()
+console.log(champ.test())
 
 // Store demo
 
