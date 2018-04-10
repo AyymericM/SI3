fc5f55f6d08b725cb7be71d862726f5ac2b7538e
AymericM <aymeric.moehn@gmail.com>
2018-04-10 17:15:00 +0200

    ufo z-index

--- js/classes/game.js
+++ js/classes/game.js
@@ -7,10 +7,13 @@ export default class Game {
 
     createPlatform() {
         const platform = document.createElement('div')
+        const platformBody = document.createElement('div')
         const hitbox = document.createElement('div')
         platform.classList.add('platform')
+        platformBody.classList.add('platformBody')
         hitbox.classList.add('platformHitBox')
         root.appendChild(platform)
         root.appendChild(hitbox)
+        root.appendChild(platformBody)
     }
 }
