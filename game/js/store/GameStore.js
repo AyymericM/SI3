299d85aeb424536ff9179da40f33ca600dde4f05
AymericM <aymeric.moehn@gmail.com>
2018-04-12 16:16:44 +0200

    keycode fix

--- game/js/store/GameStore.js
+++ game/js/store/GameStore.js
@@ -2,55 +2,55 @@ const keys = {
     mac: {
         press: {
             JUMP1: 122,
-            FIRE1: 103,
-            BALL1: 104,
-            JUMP2: 58,
-            FIRE2: 61,
+            FIRE1: 101,
+            BALL1: 97,
+            JUMP2: 111,
+            FIRE2: 105,
+            BALL2: 112
         },
         down: {
             LEFT1: 81,
             RIGHT1: 68,
-            LEFT2: 37,
-            RIGHT2: 39,
+            LEFT2: 75,
+            RIGHT2: 77,
         },
         up: {
             LEFT1: 81,
             RIGHT1: 68,
-            LEFT2: 37,
-            RIGHT2: 39,
+            LEFT2: 75,
+            RIGHT2: 77,
             JUMP1: 90,
-            BALL1: 72,
-            FIRE1: 71,
-            JUMP2: 186,
-            FIRE2: 187
+            BALL1: 65,
+            FIRE1: 69,
+            JUMP2: 79,
+            FIRE2: 73
         }
     },
     win: {
         press: {
             JUMP1: 122,
-            FIRE1: 103,
-            BALL1:104,
-            BALL2: 59,
-            JUMP2: 58,
-            FIRE2: 33,
+            FIRE1: 101,
+            BALL1: 97,
+            JUMP2: 111,
+            FIRE2: 105,
+            BALL2: 112
         },
         down: {
             LEFT1: 81,
             RIGHT1: 68,
-            LEFT2: 37,
-            RIGHT2: 39,
+            LEFT2: 75,
+            RIGHT2: 77,
         },
         up: {
             LEFT1: 81,
             RIGHT1: 68,
-            LEFT2: 37,
-            RIGHT2: 39,
+            LEFT2: 75,
+            RIGHT2: 77,
             JUMP1: 90,
-            FIRE1: 71,
-            BALL1: 72,
-            BALL2: 190,
-            JUMP2: 191,
-            FIRE2: 223
+            BALL1: 65,
+            FIRE1: 69,
+            JUMP2: 79,
+            FIRE2: 73
         }
     }
 }
